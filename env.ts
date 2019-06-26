15e7bd7cdbc717ff44023659027f448a8647dbb1
Tomer <5922086+LTomer@users.noreply.github.com>
2019-06-26 15:04:42 +0300

    Fix Environment Variables view with typescript (Linux OS)

--- env.ts
+++ env.ts
@@ -1,8 +1,20 @@
 import tl = require('vsts-task-lib/task');
+const _ = require('lodash');
 
 function run() {
-	console.log(process.env);
+	let maxSize=0;
+	_.each(process.env, function (value, key) {
+        if(key.length > maxSize)
+			maxSize = key.length;
+    });
 	
+	var maxSpace = maxSize + 3;
+	
+	_.each(process.env, (value, key) => {
+		var spaces = Array(maxSpace - key.length).join(' ')
+		
+		console.log(key + spaces + process.env[key]);
+	});
 }
 
 run();
