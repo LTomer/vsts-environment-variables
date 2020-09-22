2b65c1b530010348298af6b455747aa1b7fb779d
Tomer <5922086+LTomer@users.noreply.github.com>
2020-09-22 13:52:39 +0300

    Update task to Node10, print env by order (a-z), print post job if there is an error or input flag is set

--- env.ps1
+++
@@ -1,5 +0,0 @@
-#tfx build tasks upload --task-path ./Vault
-[CmdletBinding()]
-param()
-
-get-childitem -Path env:* | Format-Table -AutoSize
--- env.ts
+++ env.ts
@@ -1,20 +1,7 @@
-import tl = require('vsts-task-lib/task');
-const _ = require('lodash');
+import util = require("./util.js");
 
 function run() {
-	let maxSize=0;
-	_.each(process.env, function (value, key) {
-        if(key.length > maxSize)
-			maxSize = key.length;
-    });
-	
-	var maxSpace = maxSize + 3;
-	
-	_.each(process.env, (value, key) => {
-		var spaces = Array(maxSpace - key.length).join(' ')
-		
-		console.log(key + spaces + process.env[key]);
-	});
+	util.PrintVariable()
 }
 
 run();
